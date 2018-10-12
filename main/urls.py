de14c2662a31a2e9a7aac47c679196cf2471e3b8
A1H1 <a8691258@gmail.com>
2018-10-12 23:03:32 +0530

    login and signup

--- main/urls.py
+++ main/urls.py
@@ -3,15 +3,12 @@ from . import views
 
 urlpatterns = [
 
-    path('',views.index,name = 'index'),
-    path('accounts/login/',views.login_view,name = 'login'),
-    path('accounts/logout/',views.logout_view,name = 'logout'),
-    path('signup/',views.Signup.as_view(),name = 'signup'),
+    path('', views.index, name='index'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
+    path('signup/', views.Signup.as_view(), name='signup'),
     path('addrestaurant/', views.AddRestaurant.as_view(), name='addrestaurant'),
-    path('success/',views.SuccessView.as_view(),name = 'success'),
-    path('restaurants/',views.RestaurantList.as_view(),name = 'restaurants'),
-    path('restaurant/<int:pk>',views.RestaurantDetail.as_view(),name = 'restaurant'),
-
-
-
+    path('success/', views.SuccessView.as_view(), name='success'),
+    path('restaurants/', views.RestaurantList.as_view(), name='restaurants'),
+    path('restaurant/<int:pk>', views.RestaurantDetail.as_view(), name='restaurant'),
 ]
