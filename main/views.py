d3bbc563e7aefec1e3ec53ed842f5f9099b31e84
A1H1 <a8691258@gmail.com>
2018-10-12 19:27:37 +0530

    bck

--- main/views.py
+++ main/views.py
@@ -1,24 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.views.generic import TemplateView,UpdateView,DetailView,DeleteView,FormView,CreateView,ListView
-from django.views.generic.edit import FormMixin
-from django.urls import reverse,reverse_lazy
 from django import forms
-from django.contrib.auth import login,logout,authenticate
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from . import forms,models
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Avg
-from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView, DetailView, CreateView, ListView
+from django.views.generic.edit import FormMixin
+
+from . import forms, models
+
 
 class SuccessView(TemplateView):
     template_name = 'main/success.html'
 
-def login_view(request):
 
+def login_view(request):
     if request.method == "GET":
         form = forms.LoginForm()
     else:
@@ -27,7 +26,7 @@ def login_view(request):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
 
-            user = authenticate(request, username = username, password = password)
+            user = authenticate(request, username=username, password=password)
 
             if user is not None:
                 login(request, user)
@@ -35,29 +34,33 @@ def login_view(request):
                 try:
                     return HttpResponseRedirect(request.GET['next'])
                 except KeyError:
-                    return HttpResponseRedirect( reverse('index') )
+                    return HttpResponseRedirect(reverse('index'))
     context = {
         "form": form
     }
     return render(request, 'main/login.html', context)
 
+
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect( reverse('login') )
+    return HttpResponseRedirect(reverse('login'))
+
 
 class Signup(CreateView):
     form_class = UserCreationForm
-    success_url =reverse_lazy('login')
+    success_url = reverse_lazy('login')
     template_name = 'main/signup.html'
 
-class Addrestaurant(CreateView):
+
+class AddRestaurant(CreateView):
     model = models.Restaurant
     fields = '__all__'
     success_url = '/success/'
     template_name = 'main/addrestaurant.html'
 
-@method_decorator(login_required, name = 'dispatch')
-class RestaurantDetail(FormMixin,DetailView):
+
+@method_decorator(login_required, name='dispatch')
+class RestaurantDetail(FormMixin, DetailView):
     template_name = 'main/restaurant_detail.html'
     model = models.Restaurant
     context_object_name = 'restaurant'
@@ -66,7 +69,7 @@ class RestaurantDetail(FormMixin,DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form']=self.get_form()
+        context['form'] = self.get_form()
         return context
 
     def post(self,request,*args,**kwargs):
@@ -79,27 +82,20 @@ class RestaurantDetail(FormMixin,DetailView):
         return HttpResponseRedirect(redirect_to='/success/')
 
     def form_valid(self, form):
-        obj = form.save(commit = False)
+        obj = form.save(commit=False)
         obj.restaurant = self.get_object()
         obj.save()
         return super().form_valid(form)
 
 
-
-
-
-# @method_decorator(login_required,name = 'dispatch')
-# class Index(TemplateView):
-#     template_name = 'main/index.html'
-#     if request.user.is_authenticated:
-#         username = request.user.username
 @login_required
 def index(request):
     username = request.user.username
-    context ={
-        'username':username,
+    context = {
+        'username': username,
     }
-    return render(request,'main/index.html',context)
+    return render(request, 'main/index.html', context)
+
 
 class RestaurantList(ListView):
     paginate_by = 10
@@ -113,7 +109,6 @@ class RestaurantList(ListView):
         self.f = request.GET['filter']
         self.r = request.GET['reverse']
 
-
         if self.f == 'a2z' and self.r == 'False':
             self.qs = self.qs.order_by('name')
         elif self.f == 'a2z' and self.r == 'True':
@@ -124,8 +119,6 @@ class RestaurantList(ListView):
             self.qs = self.qs.annotate(average_rating=Avg('review__rating')).order_by('-average_rating')
 
         return super().get(request)
-
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
